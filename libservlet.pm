65d6c7e8fce3388201e1426222e05f85a10109a7
Brian Moseley <cpan@maz.org>
2001-06-11 01:34:48 +0000

    Import of IX/Bundle-libservlet-0.9.1 from CPAN.
    gitpan-cpan-distribution: Bundle-libservlet
    gitpan-cpan-version:      0.9.1
    gitpan-cpan-path:         IX/Bundle-libservlet-0.9.1.tar.gz
    gitpan-cpan-author:       IX
    gitpan-cpan-maturity:     released

---
+++ libservlet.pm
@@ -0,0 +1,35 @@
+# -*- Mode: Perl; indent-tabs-mode: nil; -*-
+
+package Bundle::libservlet;
+
+require 5.6;
+
+our $VERSION = '0.9.1';
+
+1;
+__END__
+
+=pod
+
+=head1 NAME
+
+Bundle::libservlet - a bundle to install all libservlet related modules
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::libservlet'>
+
+=head1 CONTENTS
+
+Exception::Class -      for Servlet::Util::Exception
+
+=head1 DESCRIPTION
+
+This bundle includes the libservlet distribution as well as
+B<Exception::Class>.
+
+=head1 AUTHOR
+
+Brian Moseley, bcm@maz.org
+
+=cut
